8868d72c7713b239d4927cc4058691c9acd87373
Javier Rodríguez <javier.rodriguez@create-store.com>
2025-08-05 12:44:55 +0200

    ADD: json response test

--- dartify/views.py
+++ dartify/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the dartify index.")
+
+def test(request):
+    return JsonResponse({"status": "ok", "message": "Everything is fine!"})
