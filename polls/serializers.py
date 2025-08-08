de30a69b417e8da75085183883c1a9c61dba05c2
Javi Rodríguez <falces@gmail.com>
2025-08-08 13:51:14 +0200

    ADD: Create parent and child entities

--- polls/serializers.py
+++ polls/serializers.py
@@ -5,11 +5,20 @@ from .models import Question, Choice
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
-        fields = '__all__'
         fields = ['id', 'choice_text', 'votes']
+        read_only_fields = ['question']
         
 class QuestionSerializer(serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True, read_only=True)
+    choices = ChoiceSerializer(many=True)
     class Meta:
         model = Question
         fields = '__all__'
+        extra_fields = ['choices']
+        
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices')
+        question = Question.objects.create(**validated_data)
+        for choice in choices_data:
+            Choice.objects.create(question=question, **choice)
+        return question
+        
--- polls/views/apiviews.py
+++ polls/views/apiviews.py
@@ -10,9 +10,9 @@ class QuestionsView(APIView):
         serializer = QuestionSerializer(questions, many=True)
         return Response(serializer.data)
 
-    # def post(self, request):
-    #     serializer = QuestionSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=201)
-    #     return Response(serializer.errors, status=400)
+    def post(self, request):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
